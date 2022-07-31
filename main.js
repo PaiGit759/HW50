1a664dd5cf28ade387422aed84fb79d2047f0b1c
Pai <paileleka@gmail.com>
2022-07-31 15:18:47 +0200

    20220731 15:18

--- main.js
+++ main.js
@@ -10,8 +10,14 @@ a-=4;
 
 a%=5;
 
+var boxElem = document.activeElement.querySelector(".exsample");
+
+boxElem.innerHTML = boxElem.innerHTML + "" + " a=5; a+=a; a*=2; a/=3; a/=3; a%=5;";
 
 var boxElem = document.activeElement.querySelector(".result");
 
-    boxElem.innerHTML = boxElem.innerHTML + a;
+boxElem.innerHTML =  a;
+
+
+
 
